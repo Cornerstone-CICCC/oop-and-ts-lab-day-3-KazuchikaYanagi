afc36bd705162c94c3c6f82e49221dcf857e1810
Kazuchika Yanagi <yanagikazuchika@gmail.com>
2024-10-18 17:04:57 -0700

    lab answer

--- exercises/ex02.js
+++ exercises/ex02.js
@@ -1,10 +1,29 @@
 // Create a function called removeBetween that removes all elements between two unique elements
 // Make sure to implement the Stack principle (LIFO)
 
-const Stack = require('../lib/Stack');
+const Stack = require("../lib/Stack");
 
 function removeBetween(a, b) {
   // your code here
+  const tempStack = new Stack();
+  const rmArr = [];
+
+  while (!fruits.isEmpty()) {
+    const el = fruits.pop();
+    tempStack.push(el);
+    if (el === a) break;
+  }
+
+  while (!tempStack.isEmpty()) {
+    const el = tempStack.pop();
+    if (el === a || el === b) {
+      fruits.push(el);
+    } else {
+      rmArr.push(el);
+    }
+  }
+
+  return fruits;
 }
 
 const fruits = new Stack();
@@ -15,4 +34,4 @@ fruits.push("Date");
 fruits.push("Elderberry");
 
 removeBetween("Banana", "Elderberry");
-fruits.printStack(); // Apple Banana Elderberry
+console.log(fruits.printStack()); // Apple Banana Elderberry
--- exercises/ex07.js
+++ exercises/ex07.js
@@ -1,17 +1,33 @@
 // Create a function called findMiddle that returns the middle node of a singly linked list
 // Tip: Create two runners (one runner moves one node at a time, the other one moves two nodes at a time)
 
-const SLL = require('../lib/SLL');
+const SLL = require("../lib/SLL");
 
 function findMiddle(list) {
   // your code here
+  let len = list.length();
+  let mdlIdx = Math.floor(len / 2);
+  let runner = list.getHead();
+  let curIdx = 0;
+
+  while (curIdx < mdlIdx) {
+    runner = runner.next;
+    curIdx++;
+  }
+
+  return runner.data;
 }
 
 const list = new SLL();
 list.insertAtBack(1);
+console.log(list);
 list.insertAtBack(2);
+console.log(list);
 list.insertAtBack(3);
+console.log(list);
 list.insertAtBack(4);
+console.log(list);
 list.insertAtBack(5);
+console.log(list);
 
 console.log(findMiddle(list)); // Output: 3
